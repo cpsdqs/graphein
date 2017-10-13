402a6c91864034c72be827f24ac75dc5f13af5c7
cpsdqs <cpsdqs@gmail.com>
2017-10-13 21:01:02 +0200

    Remove unnecessary line in transform matrix as specified in the spec

--- src/transform.js
+++ src/transform.js
@@ -10,18 +10,18 @@ module.exports = class Transform {
 
   render (ctx) {
     if (this.type === Transform.types.MAT3) {
-      ctx.transform(this.data[0], this.data[1], this.data[3], this.data[4], this.data[6], this.data[7])
+      ctx.transform(...this.data)
     } else if (this.type == Transform.types.MAT4) {
       console.warn('Cannot properly render Mat4 on canvas')
-      ctx.transform(this.data[0], this.data[1], this.data[4], this.data[5], this.data[12], this.data[13])
+      ctx.transform(this.data[0], this.data[1], this.data[3], this.data[4], this.data[9], this.data[10])
     }
   }
 
   static deserialize (data) {
     let transform = new Transform()
-    transform.type = data.length === 16
+    transform.type = data.length === 12
       ? Transform.types.MAT4
-      : data.length === 9
+      : data.length === 6
       ? Transform.types.MAT3
       : Transform.types.NONE
     transform.data = new Float32Array(data.slice())
